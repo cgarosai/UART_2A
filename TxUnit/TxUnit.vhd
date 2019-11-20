0e6e87cc023f57b9ed7acaaf55f408070fa7ebff
jdides <justindides@etu.enseeiht.fr>
2019-11-20 18:06:22 +0100

    Début du TxUnit

--- TxUnit/TxUnit.vhd
+++ TxUnit/TxUnit.vhd
@@ -13,13 +13,45 @@ entity TxUnit is
 end TxUnit;
 
 architecture behavorial of TxUnit is
+
 type t_etat is (IDLE, CHARGER_REGISTRE, ENVOI_START, 
 					ENVOI_DATA, ENVOI_PAIRE, ENVOI_STOP);  
 					
 signal etat : t_etat;
 
 
-
 begin
 
+process(clk)
+variable buf : std_logic_vector(7 downto 0);
+variable reg : std_logic_vector(7 downto 0);
+begin
+	if(reset = '0') then
+		etat <= IDLE;
+		bufE <= '1';
+		regE <= '1';
+		
+	elsif(rising_edge(clk)) then
+		
+		case etat is
+		when IDLE => 
+			bufE <= '1';
+			regE <= '1';
+			if(enable = '1' and ld = '1') then 
+				etat <= CHARGER_REGISTRE;
+				bufE <= '0';
+				buf <= data;
+			end if;
+		
+		when CHARGER_REGISTRE => 
+			if(ld = 0 and enable = '1') then
+				bufE = '1';
+				regE = '0';
+				reg <= buf;
+				etat <= ENVOI_START;
+			end if;
+			
+		end case;
+	end if;
+end process;
 end behavorial;
