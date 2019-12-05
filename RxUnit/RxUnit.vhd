65c8e0ce6f0eee5c80affd2f27dbca863acff46c
jdides <justindides@etu.enseeiht.fr>
2019-12-05 13:39:09 +0100

    Correction UCF

--- RxUnit/RxUnit.vhd
+++ RxUnit/RxUnit.vhd
@@ -12,100 +12,10 @@ entity RxUnit is
 end RxUnit;
 
 architecture behavorial of RxUnit is
-
-signal tmpclk : std_logic; -- Signaux intermédiaires de clk et de rxd venant du Proc_cpt16 vers le Proc_ctrlReception
-signal tmprxd : std_logic;
--- Etat du process cpt16
-type t_etat_cpt is (Idle, cpt1, cpt2); 
--- Etat de base : Idle, quand Rxd passe à 0, on commence la réception (11 fois cpt1 et cpt2 pour réceptionner toute la trame)
--- Cpt1 : Première phase de comptage de 8 enable, on affcte tmpclk et tmprxd à la fin du comptage.
--- Cpt2 : Deuxième phase de comptage, si on a vu toute la trame passée à la fin de cette phase
--- on va soit retourné en idle, soit relancé la réception d'une trame avec cpt1.
-signal etatCpt : t_etat_cpt;
-
 begin
 
-Proc_Cpt16 : process(enable)
-variable cpt : natural; -- Permet de compter les fronts d'enable
-variable cptTrame : natural; -- Permet de compter les bits reçut jusqu'à la fin d'une trame.
--- Taille d'une trame : 1 bit start + 8 bit donnée + 1 parité + 1 stop = 11 bits.
-begin 
-	if(reset = 0) then
-		etatCpt <= Idle;
-	elsif(rising_edge(enable)) then
-		case etatCpt is 
-		when Idle => 
-			tmpclk <= 0;
-			tmprxd <= 1;
-			cpt := 8;
-			cptTrame := 11;
-			if(rxd = '0') then 
-				etatCpt <= cpt1;
-			end if;
-			
-		when cpt1 =>
-			cpt := cpt - 1;
-			if(cpt = 0) then 
-				etatCpt <= cpt2;
-				tmpclk <= '1';
-				tmprxd <= rxd;
-				cpt := 8;
-			end if;
-			
-		when cpt2 =>
-		if(tmpclk = '1') then -- On rabaisse le signal tmpclk. 
-			tmpclk <= '0';
-		end if;
-		
-		cpt := cpt - 1;
-		
-		if(cpt = 0) then -- Fin du comptage, on repasse à cpt1 en décrémenter le nombre de bits encore à recevoir.
-			cptTrame := cptTrame - 1;
-			etatCpt <= cpt1;
-		end if;
-		
-		if(cptTrame = 0) then -- Si on a reçu toute la trame (11 bits)
-			cptTrame := 11;
-			if(rxd = '1') then -- On va soit retourner à Cpt1 si une autre transmission suit, c'est à dire 
-				etatCpt <= Idle; -- si on recoit un autre bit de start (rxd = '0') dès la fin de la trame.
-			end if; -- Ou si Rxd = '1', on va alors repasser en idle qui attendra la réception d'une future trame.
-		end if; 
-	end if;
-end process;
-
-Proc_ctrlReception : process(clk)
-
-variable regRecep : std_logic_vector(10 downto 0); -- Le registre sauvegardant la réception des 11 bits d'une trame.
-variable cptBit : natural; -- Numérotation du bit qui est en réception.
-
-type t_etat_recep is (Idle, Reception); 
-
-signal etatRecep : t_etat_cpt;
-
-begin 
-	if(reset = 0) then
-		etatRecep <= Idle;
-	elsif(rising_edge(clk)) then
-		case etatRecep is 
-		when Idle =>
-			DRdy <= '0';
-			Ferr <= '0';
-			OErr <= '0';
-			if(tmpclk = '1') then
-				etatRecep <= Reception;
-				cptBit := 11;
-			end if;	
-				
-		when Reception =>
-			if(tmpclk = '1') then 
-				cptBit := cptBit - 1;
-				regRecep(cptBit) := rxdtmp;
-				-- Bit de poids fort ou faible en premier ???? 
-				if(cptBit = 0) then 
-				-- Verif des bits de stop/parité
-				-- Envoi de la data si c'est bon.
-				
-	end if;
-end process; 
-
+	Ferr <= '0';
+	OErr <= '0';
+	DRdy <= '0';
+	data <= "00000000";
 end behavorial;
--- UART/UART.vhd
+++ UART/UART.vhd
@@ -19,13 +19,67 @@ architecture UARTunit_arch of UARTunit is
 
   -- a completer avec l'interface des differents composants
   -- de l'UART
+  	COMPONENT RxUnit
+	PORT(
+		clk : IN std_logic;
+		reset : IN std_logic;
+		enable : IN std_logic;
+		rd : IN std_logic;
+		rxd : IN std_logic;          
+		data : OUT std_logic_vector(7 downto 0);
+		Ferr : OUT std_logic;
+		OErr : OUT std_logic;
+		DRdy : OUT std_logic
+		);
+	END COMPONENT;
+	
+	-- TxUnit
+	COMPONENT TxUnit
+	PORT(
+		clk : IN std_logic;
+		reset : IN std_logic;
+		enable : IN std_logic;
+		ld : IN std_logic;
+		data : IN std_logic_vector(7 downto 0);          
+		txd : OUT std_logic;
+		regE : OUT std_logic;
+		bufE : OUT std_logic
+		);
+	END COMPONENT;
+	
+	COMPONENT clkUnit
+	PORT(
+		clk : IN std_logic;
+		reset : IN std_logic;          
+		enableTX : OUT std_logic;
+		enableRX : OUT std_logic
+		);
+	END COMPONENT;
+	
+	COMPONENT ctrlUnit
+	PORT(
+		clk : IN std_logic;
+		reset : IN std_logic;
+		rd : IN std_logic;
+		cs : IN std_logic;
+		DRdy : IN std_logic;
+		FErr : IN std_logic;
+		OErr : IN std_logic;
+		BufE : IN std_logic;
+		RegE : IN std_logic;          
+		IntR : OUT std_logic;
+		IntT : OUT std_logic;
+		ctrlReg : OUT std_logic_vector(7 downto 0)
+		);
+	END COMPONENT;
 
   signal lecture, ecriture : std_logic;
   signal donnees_recues : std_logic_vector(7 downto 0);
   signal registre_controle : std_logic_vector(7 downto 0);
 
   -- a completer par les signaux internes manquants
-
+	--signal data : std_logic_vector(7 downto 0);
+	signal EnableTx, EnableRx, DRdy, Ferr, OErr, regE, bufE : std_logic;
   begin  -- UARTunit_arch
 
     lecture <= '1' when cs = '0' and rd = '0' else '0';
@@ -35,5 +89,49 @@ architecture UARTunit_arch of UARTunit is
                 else "00000000";
   
     -- a completer par la connexion des differents composants
-
+	Inst_RxUnit: RxUnit PORT MAP(
+		clk => clk,
+		reset => reset,
+		enable => EnableRx,
+		rd => lecture,
+		rxd => RxD,
+		data => donnees_recues,
+		Ferr => Ferr,
+		OErr => OErr,
+		DRdy => DRdy
+	);
+	
+	Inst_TxUnit: TxUnit PORT MAP(
+		clk => clk,
+		reset => reset,
+		enable => EnableTx,
+		ld => ecriture,
+		txd => TxD,
+		regE => regE,
+		bufE => bufE,
+		data => data_in
+	);
+	
+	Inst_clkUnit: clkUnit PORT MAP(
+		clk => clk,
+		reset => reset,
+		enableTX => EnableTx,
+		enableRX => EnableRx
+	);
+	
+	Inst_ctrlUnit: ctrlUnit PORT MAP(
+		clk => clk,
+		reset => reset,
+		rd => cs,
+		cs => rd,
+		DRdy => DRdy,
+		FErr => Ferr,
+		OErr => OErr,
+		BufE => bufE,
+		RegE => regE,
+		IntR => IntR,
+		IntT => IntT,
+		ctrlReg => registre_controle
+	);
+	
   end UARTunit_arch;
